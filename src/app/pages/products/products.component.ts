7ecf6effee7d8bd21c1f33a7219da2435b2eb34c
Giorgi <giorgibegii@gmail.com>
2025-09-24 16:58:12 +0400

    productsPage categories modals

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProductCardComponent, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  filterModal = false
+  sortModal = false
+
+  toggleFilterModal() {
+    this.filterModal = !this.filterModal
+    if(this.sortModal === true){
+      this.sortModal = !this.sortModal
+    }
+  }
+
+  toggleSortModal() {
+    this.sortModal = !this.sortModal
+    if(this.filterModal === true){
+      this.filterModal = !this.filterModal
+    }
+  }
 }
